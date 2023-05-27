0be18ad0ba9710a2c046c1dd4a8aadf16ae5dd6b
Jefferson Sousa <jefferson.mello8@hotmail.com>
2023-05-27 13:36:16 -0300

    uploading the project

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Jefferson'
+    },
+    {
+        id: 2,
+        name: 'Rebeca'
+    },
+    {
+        id: 3,
+        name: 'Virgínia'
+    },
+]
